ec111c983451256475674c2797a37c275d757c03
michelyaogermano <118003658+michelyaogermano@users.noreply.github.com>
2025-07-03 16:23:11 -0300

    Update /dev app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,8 +4,8 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
-app.get('/mars', function(req, res) {
-  res.send('Hello..........Mars!\n');
+app.get('/mars/dev', function(req, res) {
+  res.send('Hello..Mars...dev!\n');
 });
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
