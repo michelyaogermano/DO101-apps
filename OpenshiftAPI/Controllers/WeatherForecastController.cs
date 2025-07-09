4c181a93e0ae1198c1e5695c4f497d0437d46233
michelyaogermano <118003658+michelyaogermano@users.noreply.github.com>
2025-07-09 11:00:14 -0300

    Update WeatherForecastController.cs

--- OpenshiftAPI/Controllers/WeatherForecastController.cs
+++ OpenshiftAPI/Controllers/WeatherForecastController.cs
@@ -1,15 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace OpenShiftApi.Controllers
+namespace OpenshiftAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("/")]
-    public class HelloController : ControllerBase
+    private static readonly string[] Summaries = new[]
     {
-        [HttpGet]
-        public IActionResult Get()
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            return Ok("Hello from OpenShift!");
-        }
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
     }
 }
