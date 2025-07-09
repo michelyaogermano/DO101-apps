7e921a79c46acbfc391f569700f0f53786e066d6
michelyaogermano <118003658+michelyaogermano@users.noreply.github.com>
2025-07-09 09:56:47 -0300

    Update Program.cs

--- my-app/Program.cs
+++ my-app/Program.cs
@@ -1,2 +1,6 @@
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(8080); 
+});
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,.......... World!");
