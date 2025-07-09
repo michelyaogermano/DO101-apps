3c45a4344fb708fdb3ef2e064a2567d501a5c3ca
michelyaogermano <118003658+michelyaogermano@users.noreply.github.com>
2025-07-09 09:29:54 -0300

    Update Program.cs

--- my-app/Program.cs
+++ my-app/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+builder.WebHost.UseUrls("http://+:8080");
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,.......... World!");
