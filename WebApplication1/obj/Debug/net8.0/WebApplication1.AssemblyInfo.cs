428cc1c2cf7a76fd06b4e34f3b035a3b84013e11
michelyaogermano <michelyaogermano@gmail.com>
2025-07-14 18:18:44 -0300

    do

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea654ae0aebd05e505fecf3d757625835df2d65b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff74d13aee028fe65fccc52669ea8c445499cf8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
